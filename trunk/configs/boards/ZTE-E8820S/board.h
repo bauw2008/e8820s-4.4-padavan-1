45a7c95ddb43245bf226adb5b6557786c0b1fac7
bauw2008 <77882800+bauw2008@users.noreply.github.com>
2022-10-26 13:49:20 +0800

    Update board.h

--- trunk/configs/boards/ZTE-E8820S/board.h
+++ trunk/configs/boards/ZTE-E8820S/board.h
@@ -10,6 +10,7 @@
 #define BOARD_FLASH_TIME   	120
 #define BOARD_GPIO_LED_net0 13
 #define BOARD_GPIO_LED_net1 15
+#define BOARD_GPIO_LED_SYS	16
 #define BOARD_HAS_5G_11AC	1
 #define BOARD_NUM_ANT_5G_TX	2
 #define BOARD_NUM_ANT_5G_RX	2
