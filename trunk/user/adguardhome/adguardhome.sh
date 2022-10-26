b468e5dbee72b51f0c686c4508d0638881a8639f
bauw2008 <77882800+bauw2008@users.noreply.github.com>
2022-10-26 13:12:42 +0800

    Update adguardhome.sh

--- trunk/user/adguardhome/adguardhome.sh
+++ trunk/user/adguardhome/adguardhome.sh
@@ -143,20 +143,24 @@ fi
 
 
 start_adg(){
-    mkdir -p /opt/tmp/AdGuardHome
+    mkdir -p /tmp/AdGuardHome
+        mkdir -p opt/tmp/AdGuardHome/data
 	mkdir -p /etc/storage/AdGuardHome
-	if [ ! -f "/opt/tmp/AdGuardHome/AdGuardHome" ]; then
-	cp /usr/bin/AdGuardHome /opt/tmp/AdGuardHome/AdGuardHome
+	if [ ! -f "/tmp/AdGuardHome/AdGuardHome" ]; then
+	cp /opt/adg/AdGuardHome /tmp/AdGuardHome/AdGuardHome
+	if [ ! -f "/opt/adg/AdGuardHome/data" ]; then
+	cp /tmp/AdGuardHome/data /opt/adg/AdGuardHome/data
+	chmod 777 /tmp/AdGuardHome/AdGuardHome
 	fi
 	getconfig
 	change_dns
 	set_iptable
 	logger -t "AdGuardHome" "运行AdGuardHome"
-	eval "/opt/tmp/AdGuardHome/AdGuardHome -c $adg_file -w /opt/tmp/AdGuardHome -v" &
+	eval "/tmp/AdGuardHome/AdGuardHome -c $adg_file -w /tmp/AdGuardHome -v" &
 
 }
 stop_adg(){
-rm -rf /opt/tmp/AdGuardHome
+rm -rf /tmp/AdGuardHome
 killall -9 AdGuardHome
 del_dns
 clear_iptable
