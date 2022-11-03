6573b5118cf82e626d65709a557dfb5ddf4de354
bauw2008 <77882800+bauw2008@users.noreply.github.com>
2022-11-03 14:22:41 +0800

    Update adguardhome.sh

--- trunk/user/adguardhome/adguardhome.sh
+++ trunk/user/adguardhome/adguardhome.sh
@@ -146,7 +146,7 @@ start_adg(){
         mkdir -p /opt/tmp/AdGuardHome
 	mkdir -p /etc/storage/AdGuardHome
 	if [ ! -f "/opt/tmp/AdGuardHome/AdGuardHome" ]; then
-	cp /media/AiDisk_a1opt/adg/AdGuardHome /opt/tmp/AdGuardHome/AdGuardHome
+	cp /media/AiDisk_a1/opt/adg/AdGuardHome /opt/tmp/AdGuardHome/AdGuardHome
 #       cp /opt/adg/AdGuardHome /opt/tmp/AdGuardHome/AdGuardHome
 	chmod 777 /opt/tmp/AdGuardHome/AdGuardHome
 	fi
